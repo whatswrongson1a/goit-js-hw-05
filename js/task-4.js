a70273b01273aa6fdbc8f7c7f1b0d999f426d3a6
Sonia <168562263+whatswrongson1a@users.noreply.github.com>
2024-11-06 17:28:54 +0200

    commit

--- js/task-4.js
+++ js/task-4.js
@@ -1,5 +1,5 @@
 const getTotalBalanceByGender = (users, gender) => 
-  users.filter(user => user.gender === gender)  // правильна назва параметра
+  users.filter(user => user.gender === gender)
        .reduce((total, user) => total + user.balance, 0);
 
 const clients = [
